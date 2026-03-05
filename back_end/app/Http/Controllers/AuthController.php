5d45cc0b4096c8643b2e8447d64e0122956c9514
YOBS KOLER <yobskoler9@gmail.com>
2026-03-05 11:06:26 +0100

    new update on authcontroller,user.php,api.php

--- back_end/app/Http/Controllers/AuthController.php
+++ back_end/app/Http/Controllers/AuthController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Str;    
 
 class AuthController extends Controller
 {
@@ -17,4 +21,35 @@ class AuthController extends Controller
         $token = $user->createToken('auth_token')->plainTextToken;      
         return response()->json(['token'=> $token,'role'=>$user->role,'user'=>$user]);
     }
+
+    public function forgotPassword(Request $request){
+        $request->validate(['email'=>'required|email']);
+        $status=Password::sendResetLink($request->only('email'));
+        return $status === Password::RESET_LINK_SENT
+        ? response()->json(['message'=>'Reset link sent to your email.'])   
+        : response()->json(['error'=>'Failed to send reset link.'], 500);
+    }
+   public function resetPassword(Request $request)
+{
+    $request->validate([
+        'email' => 'required|email',
+        'token' => 'required',
+        'password' => 'required|min:8|confirmed',
+    ]);
+
+    $status = Password::reset(
+        $request->only('email', 'password', 'password_confirmation', 'token'),
+        function ($user) use ($request) {
+            $user->forceFill([
+                'password' => Hash::make($request->password)
+            ])->save();
+
+            event(new PasswordReset($user));
+        }
+    );
+
+    return response()->json([
+        'message' => __($status)
+    ]);
+}
 }
--- back_end/routes/Api.php
+++ back_end/routes/Api.php
@@ -7,3 +7,5 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::get('/test', function () {
     return response()->json(['message' => 'API OK']);
 });
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class,  'resetPassword']);
